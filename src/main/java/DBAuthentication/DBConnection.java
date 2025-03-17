ec484b5e9522fe808a04ab1048b74b6e2e86c7d1
vidhuran-s <Vidhuran2024@gmail.com>
2025-03-17 12:06:28 +0530

    Update DBConnection.java

--- src/main/java/DBAuthentication/DBConnection.java
+++ src/main/java/DBAuthentication/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/UserSpecific";
+    private static final String URL = "jdbc:mysql://localhost:3306/speeko";
     private static final String USER = "root";
-    private static final String PASSWORD = "Vidhuran@zoho2025";
+    private static final String PASSWORD = "password";
 
     public static Connection getConnection() throws SQLException {
         try {
